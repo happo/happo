b46dadffc7bb2f9c12fd1108ee9b3f1d9a481090
Joe Lencioni <joe.lencioni@gmail.com>
2025-12-05 09:36:56 -0600

    Handle 409 status codes from cancel job API endpoint
    This happens when the job has already been completed. This is normal
    behavior, so we don't want to throw here.
    
    We should possibly also handle 404 this way, which happens when no job
    is found for the pair of SHAs. I see that error reported fairly
    frequently, but I don't really understand how it comes about yet, so I
    don't want to jump into it here just yet.

--- src/network/cancelJob.ts
+++ src/network/cancelJob.ts
@@ -1,6 +1,7 @@
 import type { ConfigWithDefaults } from '../config/index.ts';
 import type { EnvironmentResult } from '../environment/index.ts';
 import type { Logger } from '../isomorphic/types.ts';
+import { ErrorWithStatusCode } from './fetchWithRetry.ts';
 import makeHappoAPIRequest from './makeHappoAPIRequest.ts';
 
 type Status = 'failure' | 'success';
@@ -16,19 +17,33 @@ export default async function cancelJob(
   { beforeSha, afterSha, link, message }: EnvironmentResult,
   logger: Logger,
 ): Promise<void> {
-  await makeHappoAPIRequest(
-    {
-      path: `/api/jobs/${beforeSha}/${afterSha}/cancel`,
-      method: 'POST',
-      body: {
-        link,
-        message,
-        project: config.project,
-        status,
+  try {
+    await makeHappoAPIRequest(
+      {
+        path: `/api/jobs/${beforeSha}/${afterSha}/cancel`,
+        method: 'POST',
+        body: {
+          link,
+          message,
+          project: config.project,
+          status,
+        },
       },
-    },
-    config,
-    { retryCount: 5 },
-    logger,
-  );
+      config,
+      { retryCount: 5 },
+      logger,
+    );
+  } catch (error) {
+    if (error instanceof ErrorWithStatusCode && error.statusCode === 409) {
+      // This API endpoint responds with a 409 when the job has already been
+      // completed. This is expected behavior and we can just log the error and
+      // continue.
+      logger.error(
+        'Skipping cancellation of Happo job because it has already been completed',
+        error,
+      );
+    } else {
+      throw error;
+    }
+  }
 }
