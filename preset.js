aeca8f5382a0e3c6ac455631c049e80d84ed9788
Joe Lencioni <joe.lencioni@gmail.com>
2026-01-07 09:29:40 -0600

    Add root-level preset for Storybook
    This seems to be a requirement for being listed in their integration
    catalog.

---
+++ preset.js
@@ -0,0 +1,5 @@
+// Root level preset for Storybook
+// https://storybook.js.org/docs/addons/integration-catalog#addons
+// https://storybook.js.org/docs/addons/writing-presets#root-level-presets
+
+export * from './dist/storybook/preset.js';
